cf894a302af86dc8eeef5b1953bf4b9604fd112f
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-06-18 16:37:31 +0530

    Create merged route for math related routes

--- src/index.ts
+++ src/index.ts
@@ -12,33 +12,39 @@ export const appRouter = router({
             text: `hello ${input ?? 'world'}`,
         };
     }),
-    square: publicProcedure.input(z.number()).query(({ input }) => {
-        return {
-            result: `Square of ${input} is ${input * input}`,
-        };
-    }),
-    sqrt: publicProcedure.input(z.number()).query(({ input }) => {
-        return {
-            result: `Square root of ${input} is ${Math.sqrt(input)}`,
-        };
-    }),
-    add: publicProcedure
-        .input(
-            z.object({
-                a: z.number(),
-                b: z.number(),
-            })
-        )
-        .query(({ input }) => {
-            return {
-                result: `${input.a} + ${input.b} = ${input.a + input.b}`,
-            };
-        }),
+
     reverse: publicProcedure.input(z.string()).query(({ input }) => {
         return {
             input,
             result: `${input.split('').reverse().join('')}`,
         };
+    }),
+
+    math: router({
+        square: publicProcedure.input(z.number()).query(({ input }) => {
+            return {
+                result: `Square of ${input} is ${input * input}`,
+            };
+        }),
+
+        sqrt: publicProcedure.input(z.number()).query(({ input }) => {
+            return {
+                result: `Square root of ${input} is ${Math.sqrt(input)}`,
+            };
+        }),
+
+        add: publicProcedure
+            .input(
+                z.object({
+                    a: z.number(),
+                    b: z.number(),
+                })
+            )
+            .query(({ input }) => {
+                return {
+                    result: `${input.a} + ${input.b} = ${input.a + input.b}`,
+                };
+            }),
     }),
 });
 
